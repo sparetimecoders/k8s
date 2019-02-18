6c0f79a2d369f810ef506c791929a7d949fa0521
Joakim Olsson <joakim@unbound.se>
2019-02-18 14:27:14 +0100

    Initial commit

---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestInvalidConfig(t *testing.T) {
+  if testing.Short() {
+    fmt.Println("Skipping test in short-mode")
+  }
+
+  _, err := ParseConfig([]byte(`
+name: es
+`))
+
+  assert.Equal(t, "Missing required value for field(s): '[DnsZone CloudLabels]'\n", err.Error())
+}
+
+func TestValidConfig(t *testing.T) {
+  if testing.Short() {
+    fmt.Println("Skipping test in short-mode")
+  }
+
+  config, err := ParseConfig([]byte(`
+name: es
+dnsZone: example.com
+masterZones:
+  - a
+  - b
+  - c
+cloudLabels:
+  environment: prod
+  organisation: dSPA
+`))
+
+  assert.Nil(t, err)
+
+  assert.Equal(t, Cluster {
+    Name: "es",
+    DnsZone: "example.com",
+    Region: "eu-west-1",
+    MasterZones: []string {"a", "b", "c"},
+    NetworkCIDR: "172.21.0.0/22",
+    Nodes: Nodes{
+      Min: 1,
+      Max: 2,
+      InstanceType: "t3.medium",
+    },
+    MasterInstanceType: "t3.small",
+    CloudLabels: map[string]string {
+      "environment": "prod",
+      "organisation": "dSPA",
+    },
+    SshKeyPath: "~/.ssh/id_rsa.pub",
+  }, config)
+}
