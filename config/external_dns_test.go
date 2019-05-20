659afc0fee7ff0e3f59b6d78a02834cbb135a89b
Peter Svensson <peter@sparetimecoders.com>
2019-05-20 11:13:05 +0200

    Updated test

--- config/external_dns_test.go
+++ config/external_dns_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestContent(t *testing.T) {
 	s, _ := ExternalDNS{Domain: "replaced_domain"}.Manifests(ClusterConfig{Name: "replaced_cluster_name", DnsZone: "dns"})
-	assert.Contains(t, s, "registry.opensource.zalan.do/teapot/external-dns:latest")
+	assert.Contains(t, s, "registry.opensource.zalan.do/teapot/external-dns:v0.5.14")
 	assert.Contains(t, s, "domain-filter=replaced_domain")
 	assert.Contains(t, s, "txt-owner-id=replaced_cluster_name.dns")
 }
